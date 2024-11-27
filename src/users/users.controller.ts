0e3ffc9b1bf21ca413756cf5f99d5eaff9d26a68
izerekerie <izereuwonkundakelia@gmail.com>
2024-11-27 12:02:25 +0200

    add: users apis

---
+++ src/users/users.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { RegisterDto } from 'src/auth/dto/register.dto';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @ApiOperation({ summary: ' find users   by role' })
+  @ApiQuery({
+    name: 'role',
+    required: false,
+    enum: ['VOLUNTEER', 'DONOR'],
+    description: 'role of users to find',
+  })
+  async findAll(@Query('role') role?: Role) {
+    if (role) {
+      return await this.usersService.findByAllRole(role);
+    }
+    return await this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return await this.usersService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return await this.usersService.update(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) {
+    return await this.usersService.remove(id);
+  }
+}
